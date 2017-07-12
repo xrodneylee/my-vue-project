6f980c48690d618f6332291bf8a80a70ad3225ed
xrodneylee <xrodneylee@gmail.com>
2017-07-13 00:39:56 +0800

    update

--- first.js
+++ first.js
@@ -11,3 +11,14 @@ var app2 = new Vue({
     message: '页面加载于 ' + new Date()
   }
 })
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: '学习 JavaScript' },
+      { text: '学习 Vue' },
+      { text: '整个牛项目' }
+    ]
+  }
+})
