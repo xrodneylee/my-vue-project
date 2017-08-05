8d02b7cc7dfe879d23e7cd49165413155f65d52d
xrodneylee <xrodneylee@gmail.com>
2017-08-05 22:56:09 +0800

    [feature] todo list

--- todo-list/todo-list.js
+++ todo-list/todo-list.js
@@ -1,6 +1,11 @@
 var app = new Vue({
-    el : '#app',
-    data : {
-        title: 'TODO LIST'
+    el: '#app',
+    data: {
+        title : 'TODO LIST',
+        todos : [],
+        newTodo: ''
+    },
+    methods: {
+        
     }
 });
