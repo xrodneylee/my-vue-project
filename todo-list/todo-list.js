435ebb2f66ac819af632e5d3265bb22327450d6f
Lee Guan Pu <xrodneylee@infinitiessoft.com>
2017-08-09 17:26:09 +0800

    nav

--- todo-list/todo-list.js
+++ todo-list/todo-list.js
@@ -3,9 +3,27 @@ var app = new Vue({
     data: {
         title : 'TODO LIST',
         todos : [],
-        newTodo: ''
+        newTodo: '',
+        showTodo: true,
+        showDoing: false,
+        showDone: false,
+        navLinkClass:'nav-link'
     },
     methods: {
-        
+        test1: function () {
+            this.showTodo= true;
+            this.showDoing= false;
+            this.showDone= false;
+        },
+        test2: function () {
+            this.showTodo= false;
+            this.showDoing= true;
+            this.showDone= false;
+        },
+        test3: function () {
+            this.showTodo= false;
+            this.showDoing= false;
+            this.showDone= true;
+        }
     }
 });
